749c73e28ea30f590af691910ed8e63c1b70ca7c
saltfactory <saltfactory@gmail.com>
2016-02-04 02:03:21 +0900

    update Oracle Enterprise Linux 7

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
-FROM saltfactory/oracle-xe:11g
+FROM saltfactory/oracle-xe
 MAINTAINER SungKwang Song <saltfactory@gmail.com>
 
 ADD ./scripts /scripts
 RUN chmod +x /scripts/start.sh
 
+ENV NLS_LANG=KOREAN_KOREA.UTF8
+ENV LANG=ko_KR.UTF-8
+
 EXPOSE 22
 EXPOSE 1521
 EXPOSE 8080
 
-ENTRYPOINT /scripts/start.sh
+CMD /scripts/start.sh
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-sed -i -E "s/HOST = [^)]+/HOST = $HOSTNAME/g" /u01/app/oracle/product/11.2.0/xe/network/admin/listener.ora; \
-service oracle-xe start
+sed -i -E "s/HOST = [^)]+/HOST = $HOSTNAME/g" $ORACLE_HOME/network/admin/listener.ora
+while true; do
+    pmon=`ps -ef | grep pmon_$ORACLE_SID | grep -v grep`
+    if [ "$pmon" == "" ]
+    then
+        date
+        /etc/init.d/oracle-xe start
 
-sh /u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh
-export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
-export ORACLE_SID=XE
-export NLS_LANG=`$ORACLE_HOME/bin/nls_lang.sh`
-export PATH=$ORACLE_HOME/bin:$PATH
+	sqlplus system/oracle @/scripts/create_user.sql
+	sqlplus tutorial/tutorial @/scripts/example.sql
+	echo "excuted sql scripts"
+    fi
+    sleep 1m
+done;
 
-sqlplus system/oracle @/scripts/create_user.sql
-sqlplus tutorial/tutorial @/scripts/example.sql
-
-/usr/sbin/sshd -D
